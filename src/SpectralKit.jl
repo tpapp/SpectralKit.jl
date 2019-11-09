019ced55bc4cdda70a4d71c2a3d7ee5f755e2a32
Tamas K. Papp <tkpapp@gmail.com>
2019-11-09 16:43:10 +0100

    Chebyshev polynomials with tests.

--- src/SpectralKit.jl
+++ src/SpectralKit.jl
@@ -1,3 +1,194 @@
 module SpectralKit
 
+export Chebyshev, SemiInfChebyshev, roots, augmented_extrema, evaluate, domain_extrema
+
+using DocStringExtensions: FUNCTIONNAME, SIGNATURES, TYPEDEF
+
+####
+#### general
+####
+
+"""
+$(TYPEDEF)
+
+Abstract type for function families.
+
+# Supported interface
+
+For `F::FunctionFamily`, the supported API functions are
+
+- [`domain_extrema`](@ref) for querying the domain,
+
+- [`evaluate`](@ref) for function evaluation,
+
+- [`roots`](@ref) and [`augmented_extrema`](@ref) to obtain collocation points.
+"""
+abstract type FunctionFamily end
+
+Broadcast.broadcastable(f::FunctionFamily) = Ref(f)
+
+"""
+`$(FUNCTIONNAME)(F::FunctionFamily, x, order)`
+
+Internal function for implementing conversion of `x` to radians according to the family `F`.
+
+`order` follows the same semantics as [`evaluate`](@ref).
+"""
+function to_radian end
+
+"""
+`$(FUNCTIONNAME)(F::FunctionFamily, θ)
+
+Internal function for implementing conversion from half-turn units (multiples of `π`) to the
+domain of the family `F`.
+"""
+function from_halfturn end
+
+####
+#### Chebyshev on [-1,1]
+####
+
+"""
+$(TYPEDEF)
+
+Chebyhev polynomials of the first kind.
+"""
+struct Chebyshev <: FunctionFamily end
+
+@inline domain_extrema(::Chebyshev) = (-1.0, 1.0)
+
+_flip_if(condition, x) = condition ? -x : x
+
+_evaluate_min(::Chebyshev, x::T, n, ::Val{1}) where {T} = _flip_if(iseven(n), T(abs2(n)))
+
+_evaluate_max(::Chebyshev, x::T, n, ::Val{1}) where {T} = T(abs2(n))
+
+to_radian(::Chebyshev, x, ::Val{0}) = acos(x)
+
+to_radian(family, x, ::Val{1}) = last(to_radian(family, x, Val{0:1}))
+
+function to_radian(::Chebyshev, x, ::Val{0:1})
+    t = acos(x)
+    t′ = 1 / sin(t)
+    t, t′
+end
+
+from_halfturn(::Chebyshev, θ) = cospi(θ)
+
+####
+#### Rational Chebyshev on [0,Inf)
+####
+
+struct SemiInfChebyshev{T} <: FunctionFamily
+    L::T
+end
+
+to_radian(TL::SemiInfChebyshev, y, ::Val{0}) = 2 * acot(√(y / TL.L))
+
+function to_radian(TL::SemiInfChebyshev, y, ::Val{0:1})
+    ryL = √(y / L)
+    t = 2 * acot(ryL)
+    t′ = -1 / (ryL * (y + L))
+    t, t′
+end
+
+from_halfturn(TL::SemiInfChebyshev, θ) = TL.L * abs2(cot(θ * π / 2))
+
+####
+#### generic API and its implementation
+####
+
+"""
+$(SIGNATURES)
+
+Helper function for [`roots`](@ref), returning the location of `N` roots as angles, in
+halfturn units.
+
+Implementation ensures that mathematical zeros are numerically `0`. `T` determines the
+resulting float type.
+
+Internal, not exported.
+"""
+function halfturn_roots(::Type{T}, N::Integer) where {T <: Real}
+    ((2 * N - 1):-2:1) ./ T(2 * N)
+end
+
+"""
+$(SIGNATURES)
+
+Helper function for [`augmented_extrema`](@ref), returning the location of `N` augmented
+extrema as angles, in halfturn units.
+
+Implementation ensures that mathematical zeros are numerically `0`. `T` determines the
+resulting float type.
+
+Internal, not exported.
+"""
+function halfturn_augmented_extrema(::Type{T}, N::Integer) where {T <: Real}
+    ((N-1):-1:0) ./ T(N - 1)
+end
+
+"""
+`$(FUNCTIONNAME)([T], family, N)`
+
+Return the roots of the `N`th function in `family`, as a vector of `N` numbers with element
+type `T` (default `Float64`).
+"""
+roots(T, family, N) = from_halfturn.(Ref(family), halfturn_roots(T, N))
+
+roots(family, N::Integer) = roots(Float64, family, N)
+
+"""
+`$(FUNCTIONNAME)([T], family, N)`
+
+Return the augmented extrema (extrema + boundary values, in increasing order) of the `N-1`th
+function in `family`, as a vector of `N` numbers with element type `T` (default `Float64`).
+"""
+function augmented_extrema(::Type{T}, family::FunctionFamily, N) where {T}
+    from_halfturn.(Ref(family), halfturn_augmented_extrema(T, N))
+end
+
+function augmented_extrema(family::FunctionFamily, N::Integer)
+    augmented_extrema(Float64, family, N)
+end
+
+"""
+$(SIGNATURES)
+
+Evaluate the `n` (starting from 0) function in `family` at `x`.
+
+`order` determines the derivatives:
+
+- `Val(d)` returns the `d`th derivatives, starting from `0` (for the function value)
+
+- `Val(0:d)` returns the derivatives up to `d`, starting from the function value, as
+  multiple values (ie a tuple).
+"""
+evaluate(family::FunctionFamily, x, n, order::Val{0}) = cos(n * to_radian(family, x, order))
+
+function evaluate(family::FunctionFamily, x, n, order::Val{1})
+    mi, ma = domain_extrema(family)
+    if x == mi
+        _evaluate_min(family, x, n, Val(1))
+    elseif x == ma
+        _evaluate_max(family, x, n, Val(1))
+    else
+        t, t′ = to_radian(family, x, Val(0:1))
+        n * sin(n * t) * t′
+    end
+end
+
+function evaluate(family::FunctionFamily, x, n, order::Val{0:1})
+    mi, ma = domain_extrema(family)
+    if x == mi
+        evaluate(family, x, n, Val(0)), _evaluate_min(family, x, n, Val(1))
+    elseif x == ma
+        evaluate(family, x, n, Val(0)), _evaluate_max(family, x, n, Val(1))
+    else
+        t, t′ = to_radian(family, x, Val(0:1))
+        s, c = sincos(n * t)
+        c, s * n * t′
+    end
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,61 @@
 using SpectralKit
 using Test
+import ForwardDiff
+
+@testset "Chebyshev" begin
+    F = Chebyshev()
+
+    @testset "roots" begin
+        r = roots(F, 11)
+        @test r isa Vector{Float64}
+        @test length(r) == 11
+        @test r[6] == 0                 # precise 0
+        @test all(abs.(evaluate.(F, r, 11, Val(0))) .≤ 1e-14)
+    end
+
+    @testset "augmented extrema" begin
+        a = augmented_extrema(F, 11)
+        @test a isa Vector{Float64}
+        @test length(a) == 11
+        @test a[6] == 0                 # precise 0
+        @test all(abs.(evaluate.(F, a[2:(end-1)], 10, Val(1))) .≤ 1e-13)
+    end
+
+    @testset "endpoints" begin
+        mi, ma = domain_extrema(F)
+        @test mi == -1
+        @test ma == 1
+        for n in 0:10
+            atol = eps()^0.25           # very crude continuity test
+
+            # at domain minimum
+            f_mi, f′_mi = @inferred evaluate(F, mi, n, Val(0:1))
+            @test f_mi isa Float64
+            @test f′_mi isa Float64
+            @test f_mi ≈ evaluate(F, mi, n, Val(0))
+            @test f_mi ≈ evaluate(F, mi + eps(), n, Val(0)) atol = atol
+            @test f′_mi ≈ evaluate(F, mi, n, Val(1))
+            @test f′_mi ≈ evaluate(F, mi + eps(), n, Val(1)) atol = atol
+
+            # at domain maximum
+            f_ma, f′_ma = @inferred evaluate(F, ma, n, Val(0:1))
+            @test f_ma isa Float64
+            @test f′_ma isa Float64
+            @test f_ma ≈ evaluate(F, ma, n, Val(0))
+            @test f_ma ≈ evaluate(F, ma - eps(), n, Val(0)) atol = atol
+            @test f′_ma ≈ evaluate(F, ma, n, Val(1))
+            @test f′_ma ≈ evaluate(F, ma - eps(), n, Val(1)) atol = atol
+        end
+    end
+
+    @testset "random evaluations and AD tests" begin
+        for _ in 1:10
+            x = rand() * 2 - 1
+            n = rand(0:10)
+            f, f′ = @inferred evaluate(F, x, n, Val(0:1))
+            @test f ≈ @inferred evaluate(F, x, n, Val(0))
+            @test f′ ≈ @inferred evaluate(F, x, n, Val(1))
+            @test f′ ≈ ForwardDiff.derivative(x -> evaluate(F, x, n, Val(0)), x)
+        end
+    end
+end
