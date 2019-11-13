0dcba30f619355127345a58d41880bd4b64294bd
Tamas K. Papp <tkpapp@gmail.com>
2019-11-13 12:38:21 +0100

    Organize tests into functions.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,60 +2,75 @@ using SpectralKit
 using Test
 import ForwardDiff
 
+####
+#### utility functions for tests
+####
+
+function test_roots(family, N; atol = 1e-14)
+    r = roots(family, N)
+    @test r isa Vector{Float64}
+    @test length(r) == N
+    @test r[(N + 1) ÷ 2] == 0   # precise 0
+    @test all(abs.(evaluate.(family, r, N, Val(0))) .≤ atol)
+end
+
+function test_augmented_extrema(family, N; atol = 1e-13)
+    a = augmented_extrema(family, N)
+    @test a isa Vector{Float64}
+    @test length(a) == N
+    @test a[(N + 1) ÷ 2] == 0 # precise 0
+    @test all(abs.(evaluate.(family, a[2:(end-1)], N - 1, Val(1))) .≤ 1e-13)
+    mi, ma = domain_extrema(family)
+    @test mi == a[1]
+    @test ma == a[end]
+end
+
+function test_endpoint_continuity(family, expected_extrema, ns;
+                                  eps_step = eps(), atol = eps()^0.25)
+    mi, ma = domain_extrema(family)
+    @test mi == expected_extrema[1]
+    @test ma == expected_extrema[2]
+    for n in ns
+        # at domain minimum
+        f_mi, f′_mi = @inferred evaluate(family, mi, n, Val(0:1))
+        @test f_mi isa Float64
+        @test f′_mi isa Float64
+        @test f_mi ≈ evaluate(family, mi, n, Val(0))
+        @test f_mi ≈ evaluate(family, mi + eps(), n, Val(0)) atol = atol
+        @test f′_mi ≈ evaluate(family, mi, n, Val(1))
+        @test f′_mi ≈ evaluate(family, mi + eps(), n, Val(1)) atol = atol
+
+        # at domain maximum
+        f_ma, f′_ma = @inferred evaluate(family, ma, n, Val(0:1))
+        @test f_ma isa Float64
+        @test f′_ma isa Float64
+        @test f_ma ≈ evaluate(family, ma, n, Val(0))
+        @test f_ma ≈ evaluate(family, ma - eps(), n, Val(0)) atol = atol
+        @test f′_ma ≈ evaluate(family, ma, n, Val(1))
+        @test f′_ma ≈ evaluate(family, ma - eps(), n, Val(1)) atol = atol
+    end
+end
+
+function test_derivatives(family, generator, ns; M = 100)
+    for n in ns
+        for _ in 1:M
+            x = generator()
+            f, f′ = @inferred evaluate(family, x, n, Val(0:1))
+            @test f ≈ @inferred evaluate(family, x, n, Val(0))
+            @test f′ ≈ @inferred evaluate(family, x, n, Val(1))
+            @test f′ ≈ ForwardDiff.derivative(x -> evaluate(family, x, n, Val(0)), x)
+        end
+    end
+end
+
+####
+#### tests
+####
+
 @testset "Chebyshev" begin
     F = Chebyshev()
-
-    @testset "roots" begin
-        r = roots(F, 11)
-        @test r isa Vector{Float64}
-        @test length(r) == 11
-        @test r[6] == 0                 # precise 0
-        @test all(abs.(evaluate.(F, r, 11, Val(0))) .≤ 1e-14)
-    end
-
-    @testset "augmented extrema" begin
-        a = augmented_extrema(F, 11)
-        @test a isa Vector{Float64}
-        @test length(a) == 11
-        @test a[6] == 0                 # precise 0
-        @test all(abs.(evaluate.(F, a[2:(end-1)], 10, Val(1))) .≤ 1e-13)
-    end
-
-    @testset "endpoints" begin
-        mi, ma = domain_extrema(F)
-        @test mi == -1
-        @test ma == 1
-        for n in 0:10
-            atol = eps()^0.25           # very crude continuity test
-
-            # at domain minimum
-            f_mi, f′_mi = @inferred evaluate(F, mi, n, Val(0:1))
-            @test f_mi isa Float64
-            @test f′_mi isa Float64
-            @test f_mi ≈ evaluate(F, mi, n, Val(0))
-            @test f_mi ≈ evaluate(F, mi + eps(), n, Val(0)) atol = atol
-            @test f′_mi ≈ evaluate(F, mi, n, Val(1))
-            @test f′_mi ≈ evaluate(F, mi + eps(), n, Val(1)) atol = atol
-
-            # at domain maximum
-            f_ma, f′_ma = @inferred evaluate(F, ma, n, Val(0:1))
-            @test f_ma isa Float64
-            @test f′_ma isa Float64
-            @test f_ma ≈ evaluate(F, ma, n, Val(0))
-            @test f_ma ≈ evaluate(F, ma - eps(), n, Val(0)) atol = atol
-            @test f′_ma ≈ evaluate(F, ma, n, Val(1))
-            @test f′_ma ≈ evaluate(F, ma - eps(), n, Val(1)) atol = atol
-        end
-    end
-
-    @testset "random evaluations and AD tests" begin
-        for _ in 1:10
-            x = rand() * 2 - 1
-            n = rand(0:10)
-            f, f′ = @inferred evaluate(F, x, n, Val(0:1))
-            @test f ≈ @inferred evaluate(F, x, n, Val(0))
-            @test f′ ≈ @inferred evaluate(F, x, n, Val(1))
-            @test f′ ≈ ForwardDiff.derivative(x -> evaluate(F, x, n, Val(0)), x)
-        end
-    end
+    test_roots(F, 11)
+    test_augmented_extrema(F, 11)
+    test_endpoint_continuity(F, (-1, 1), 0:10)
+    test_derivatives(F, () -> rand() * 2 - 1, 0:10)
 end
