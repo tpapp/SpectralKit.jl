96d842160cec470a4edd92ae964f4f1616679d2d
Tamas K. Papp <tkpapp@gmail.com>
2020-01-21 12:34:55 +0100

    commit docs directory

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using Documenter, SpectralKit
+
+makedocs(
+    modules = [SpectralKit],
+    format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
+    authors = "Tamas K. Papp",
+    sitename = "SpectralKit.jl",
+    pages = Any["index.md"],
+    # strict = true,
+    clean = true,
+    # checkdocs = :exports,
+)
+
+deploydocs(
+    repo = "github.com/tpapp/SpectralKit.jl.git",
+    push_preview = true,
+)
---
+++ docs/src/index.md
@@ -0,0 +1,20 @@
+# SpectralKit
+
+## Abstract interface for function families
+
+```@docs
+is_function_family
+domain_extrema
+evaluate
+roots
+augmented_extrema
+```
+
+## Specific function families
+
+```@docs
+Chebyshev
+ChebyshevSemiInf
+ChebyshevInf
+ChebyshevInterval
+```
