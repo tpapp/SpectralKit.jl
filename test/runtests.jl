a88c2a6f8cb474bb417db37b1216b2b346b8ce13
Tamás K. Papp <tkpapp@gmail.com>
2024-05-22 16:52:40 +0200

    add JET and Aqua testing, remove cruft from Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,3 +11,15 @@ include("test_chebyshev.jl")
 include("test_smolyak_traversal.jl")
 include("test_smolyak.jl")
 include("test_generic_api.jl")  # NOTE moved last as it used constructs from above
+
+using JET
+@testset "static analysis with JET.jl" begin
+    @test isempty(JET.get_reports(report_package(SpectralKit, target_modules=(SpectralKit,))))
+end
+
+@testset "QA with Aqua" begin
+    import Aqua
+    Aqua.test_all(SpectralKit; ambiguities = false)
+    # testing separately, cf https://github.com/JuliaTesting/Aqua.jl/issues/77
+    Aqua.test_ambiguities(SpectralKit)
+end
