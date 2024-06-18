e2fe643ab359b7f16c31b73bcdde676183134d76
Tamás K. Papp <tkpapp@gmail.com>
2024-06-18 18:04:30 +0200

    fix escape

--- src/transformations.jl
+++ src/transformations.jl
@@ -45,7 +45,7 @@ Transform `x` from `domain` using `transformation`.
 
 `domain` can be replaced by `basis` for a shortcut which uses `domain(basis)`.
 
-Transformations to infinity make sure that ``\pm\infty`` is mapped to the limit for
+Transformations to infinity make sure that ``\\pm\\infty`` is mapped to the limit for
 values and derivatives.
 """
 function transform_from end
