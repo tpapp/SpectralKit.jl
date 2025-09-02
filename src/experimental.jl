658c8cd0cc090625e7615cedb6594494d594572d
Tamás K. Papp <tkpapp@gmail.com>
2025-09-02 14:28:04 +0200

    start work on generic framework

--- src/experimental.jl
+++ src/experimental.jl
@@ -10,25 +10,44 @@ A best effort is made to
 module Experimental
 
 using Compat: @compat
+@compat public model_parameters_dimension, make_model_parameters,
+    calculate_derived_quantities, make_approximation_basis, describe_policy_transformations,
+    policy_coefficients_dimension, make_policy_functions
 
-@compat public model_parameters_dimension, make_model_parameters, calculate_derived_quantities
-
+using ArgCheck: @argcheck
 using DocStringExtensions: FUNCTIONNAME, SIGNATURES
+import ..SpectralKit
 
 ####
-#### endpoint mappings
+#### utilities
 ####
 
-# TODO
+"""
+$(SIGNATURES)
+
+FIXME docs
+"""
+function named_cumulative_ranges(lengths::NamedTuple{N}) where N
+    # NOTE this approach  relies on Base.afoldl, so it unrolled only up to 31 elements
+    ranges = accumulate(lengths, init = 0:0) do r, l
+        a = last(r)
+        (a + 1):(a + l)
+    end
+    NamedTuple{N}(ranges)
+end
 
 ####
 #### modelling API
 ####
 
+const USERNOTE = "**User should implement this method for the relevant `model_family`.**"
+
 """
 $(FUNCTIONNAME)(model_family)
 
 Dimension of the model parameters as a flat vector.
+
+$(USERNOTE)
 """
 function model_parameters_dimension end
 
@@ -36,8 +55,12 @@ function model_parameters_dimension end
 $(FUNCTIONNAME)(model_family, x::AbstractVector)
 
 Convert a flat vector of dimension [`model_parameters_dimension`)(@ref) to the model
-parameters, which can be an arbitrary value (eg a `NamedTuple`), which is not used for
-dispatch.
+parameters.
+
+$(USERNOTE)
+
+The returned value can be an arbitrary (eg a `NamedTuple`), as it does not need to be
+not used for dispatch.
 
 !!! NOTE
     The method should accept all finite numbers in ``ℝ``, and transform them accordingly.
@@ -48,7 +71,44 @@ function make_model_parameters end
 $(SIGNATURES)(model_family, model_parameters)
 
 Calculate derived quantities (for use in determining the bases and calculating the residuals).
+
+$(USERNOTE)
 """
 function calculate_derived_quantities end
+
+"""
+$(FUNCTIONNAME)(model_family, derived_quantities, approximation_scheme)
+
+Construct an approximation basis.
+
+$(USERNOTE)
+"""
+function make_approximation_basis end
+
+"""
+$(FUNCTIONNAME)(model_family)
+
+Return a `NamedTuple` is policy function approximation schemes. The values describe the
+transformations.
+
+$(USERNOTE)
+"""
+function describe_policy_transformations end
+
+function policy_coefficients_dimension(policy_transformations::NamedTuple, approximation_basis)
+    SpectralKit.dimension(approximation_basis) * length(policy_transformations)
+end
+
+function make_policy_functions(model_family, policy_transformations::NamedTuple,
+                               approximation_basis, coefficients)
+    d = SpectralKit.dimension(approximation_basis)
+    # QUESTION line below assumes all univariate, generalize?
+    ranges = named_cumulative_ranges(map(_ -> d, policy_transformations))
+    @argcheck firstindex(coefficients) == 1
+    @argcheck lastindex(coefficients) == last(last(ranges))
+    map(ranges, policy_transformations) do r, t
+        t ∘ SpectralKit.linear_combination(approximation_basis, @view coefficients[r])
+    end
+end
 
 end
--- test/test_experimental.jl
+++ test/test_experimental.jl
@@ -1,6 +1,7 @@
-import SpectralKit.Experimental as SK
-
+import SpectralKit.Experimental as SKX
 using LogExpFunctions: logistic
+using Test
+using SpectralKit
 
 """
 A simple Ramsey model in discrete time.
@@ -52,17 +53,40 @@ c_s = f(k_s) - k_s
 """
 struct RamseyDiscrete end
 
-SK.model_parameters_dimension(::RamseyDiscrete) = 3
+SKX.model_parameters_dimension(::RamseyDiscrete) = 3
 
-function SK.make_model_parameters(::RamseyDiscrete, x)
+function SKX.make_model_parameters(::RamseyDiscrete, x)
     pre_α, pre_β, pre_δ = x
-    (; a = logistic(pre_α), β = logistic(pre_β), δ = logistic(pre_δ))
+    (; α = logistic(pre_α), β = logistic(pre_β), δ = logistic(pre_δ))
 end
 
-function SK.calculate_derived_quantities(::RamseyDiscrete, model_parameters)
+product(model_parameters, k) = k^model_parameters.α  - model_parameters.δ * k
+
+function SKX.calculate_derived_quantities(::RamseyDiscrete, model_parameters)
     (; α, β, δ) = model_parameters
     # f'(k) β = (α k^{α-1} - δ) β = 1
     k_s = ((1 / β + δ) / α)^(α - 1)
-    c_s = k_s^α - δ_s * k_s - k_s
+    c_s = product(model_parameters, k_s) - k_s
     (; k_s, c_s)
 end
+
+function SKX.make_approximation_basis(::RamseyDiscrete, derived_quantities, approximation_scheme)
+    (; policy_coefficients) = approximation_scheme
+    (; k_s) = derived_quantities
+    Chebyshev(InteriorGrid(), policy_coefficients) ∘ SemiInfRational(0.0, k_s)
+end
+
+SKX.describe_policy_transformations(::RamseyDiscrete) = (; c_share = logistic)
+
+model_family = RamseyDiscrete()
+model_parameters = SKX.make_model_parameters(model_family,
+                                            randn(SKX.model_parameters_dimension(model_family)))
+derived_quantities = SKX.calculate_derived_quantities(model_family, model_parameters)
+approximation_scheme = (; policy_coefficients = 10)
+approximation_basis = SKX.make_approximation_basis(model_family, derived_quantities,
+                                                   approximation_scheme)
+policy_transformations = SKX.describe_policy_transformations(model_family)
+policy_functions = SKX.make_policy_functions(model_family, policy_transformations,
+                                             approximation_basis,
+                                             zeros(SKX.policy_coefficients_dimension(policy_transformations,
+                                                                                     approximation_basis)))
